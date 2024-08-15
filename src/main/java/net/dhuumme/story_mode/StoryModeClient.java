6423a40fb1bdd5f9c82f324ca4cd6b5352e0ada1
DhuumMe <benoit.abelard@gmail.com>
2024-08-15 15:10:31 +0200

    Initial commit

---
+++ src/main/java/net/dhuumme/story_mode/StoryModeClient.java
@@ -0,0 +1,10 @@
+package net.dhuumme.story_mode;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StoryModeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
